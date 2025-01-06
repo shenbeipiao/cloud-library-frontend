782c239a61be7863f0630efe914085273204236f
shenbeipiao <3066152151@qq.com>
2025-01-06 21:58:52 +0800

    前端初始化

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader></GlobalHeader>
+      </a-layout-header>
+      <a-layout>
+        <a-layout-content class="content">
+          <router-view />
+        </a-layout-content>
+      </a-layout>
+      <a-layout-footer class="footer">
+        哈哈
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<style scoped>
+#basicLayout .header{
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+#basicLayout .footer{
+    background: #efefef;
+    padding: 16px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+}
+</style>
